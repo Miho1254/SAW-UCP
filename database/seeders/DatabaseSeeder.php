e05a73de236f884237015cb256993f75df0f60de
Miho1254 <nguyenha280876@gmail.com>
2026-05-30 17:40:06 +0700

    fix: call ReleaseNoteSeeder and QuizQuestionSeeder from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // create 120 online player records each with a time differing by one day from the previous record
-        for ($i = 0; $i < 120; $i++) {
-            \App\Models\Misc\OnlinePlayers::factory()->create([
-                'created_at' => now()->subDays(120)->addDays($i)->format('Y-m-d')
-            ]);
-        }
+        $this->call([
+            ReleaseNoteSeeder::class,
+            QuizQuestionSeeder::class,
+        ]);
     }
 }
